2bae5ce035031b10941329f86fc9f390c3296e93
shepherrrd <shefkid2@gmail.com>
2022-12-06 18:31:47 +0100

    Full Recipy Finished

--- fullrecipy.php
+++ fullrecipy.php
@@ -44,33 +44,7 @@ $row=mysqli_fetch_array($resul);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script type="text/javascript" src="rating.js"></script>
 <script language="javascript" type="text/javascript">
-// $(function() {
-//     $("#rating_star").codexworld_rating_widget({
-//         starLength: '5',
-//         initialValue: '',
-//         callbackFunctionName: 'processRating',
-//         imageDirectory: 'images/',
-//         inputAttr: 'postID'
-//     });
-// });
 
-// function processRating(val, attrVal){
-//     $.ajax({
-//         type: 'POST',
-//         url: 'rating.php',
-//         data: 'postID='+attrVal+'&ratingPoints='+val,
-//         dataType: 'json',
-//         success : function(data) {
-//             if (data.status == 'ok') {
-//                 alert('You have rated '+val+' to ');
-//                 $('#avgrat').text(data.average_rating);
-//                 $('#totalrat').text(data.rating_number);
-//             }else{
-//                 alert('Some problem occured, please try again.');
-//             }
-//         }
-//     });
-// }
 </script>
 <style type="text/css">
     .overall-rating{font-size: 14px;margin-top: 5px;color: #8e8d8d;}
